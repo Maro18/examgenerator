1d885ac91979e372cc7002eb5d33b8d7ce29e912
Maro <maro.mercep@gmail.com>
2019-02-25 02:08:18 +0100

    testing exam

--- src/components/Exam.vue
+++ src/components/Exam.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="exam container center-align">
-        <h1>{{ name }}</h1>
         <div>{{ $route.params.id }}</div>
     </div>
 </template>
@@ -9,7 +8,52 @@
     export default {
         name: 'Exam',
         data() {
-            return {}
+            return {
+                // Get by $route.params.id
+                exam: {
+                    id: 1,
+                    name: "First exam",
+                    question_number: 3,
+                    max_score: 20,
+                    date_created: '2018-03-25',
+                    level: {
+                        id: 1,
+                        name: 'easy'
+                    },
+                    subject: {
+                        id: 1,
+                        name: 'Math'
+                    },
+                    questions: [
+                        {
+                            id: 1,
+                            text: "Prvi pitanje etc?",
+                            score: 20,
+                            subject: {
+                                id: 2,
+                                name: 'Math'
+                            },
+                            level: {
+                                id: 1,
+                                name: 'Easy'
+                            },
+                            points: 2,
+                            answers: [
+                            {
+                                id: 1,
+                                text: 'Nothing',
+                                correct: false
+                            },
+                            {
+                                id: 2,
+                                text: 'Wtf',
+                                correct: true
+                            }
+                        ]
+                       }
+                    ]
+                }
+            }
         }
     }
 </script>
